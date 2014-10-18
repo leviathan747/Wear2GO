22a1d96a396d5d1f66c49975963a695b4779108e
Levi Starrett <levi@roxsoftware.com>
2014-10-18 06:47:10 -0400

    added async get class

---
+++ SmartShirt/app/src/main/java/com/example/boilermake/smartshirt/AsyncHTTPGet.java
@@ -0,0 +1,46 @@
+package com.example.boilermake.smartshirt;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by levistarrett on 10/18/14.
+ */
+public class AsyncHTTPGet extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... urls) {
+        String response = "";
+        for (String url : urls) {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(url);
+            try {
+                HttpResponse execute = client.execute(httpGet);
+                InputStream content = execute.getEntity().getContent();
+
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                String s = "";
+                while ((s = buffer.readLine()) != null) {
+                    response += s;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Log.i("TEST", response);
+        return response;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+    }
+}
